be9530627532ccff5b0dca14a08be499f0681c31
Joao <joaopedrolombardiv@gmail.com>
2022-08-26 19:40:40 -0300

    Feat: Aplicando DesignPattern Adapter

---
+++ src/main/java/br/com/store/budget/BudgetRecord.java
@@ -0,0 +1,12 @@
+package br.com.store.budget;
+
+public class BudgetRecord {
+
+    public void register(Budget budget){
+        // chamada HTTP para API externa
+        //URL Connection
+        //HTTP Client
+        //lib Rest
+    }
+
+}
